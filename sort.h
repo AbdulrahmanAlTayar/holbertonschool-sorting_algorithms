3e1671b981ed89937ce5ab5785d2fcedabaabf9a
Abdulrahman <10939@holbertonstudents.com>
2025-06-22 16:19:40 +0300

    uploading task 0

--- sort.h
+++ sort.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
